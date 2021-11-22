c6dfc72b342cd32a714a1fed1f7236c30ad5cf2c
Moisés Topete <54329172+moises995@users.noreply.github.com>
2021-11-22 09:33:33 -0600

    CHG class Member

--- src/Model/Google/Member.php
+++ src/Model/Google/Member.php
@@ -4,46 +4,16 @@ namespace ITColima\SiitecApi\Model\Google;
 
 use JsonSerializable;
 
-Class Member implements JsonSerializable
+Class Member
 {
+    /** @var string */
     public $email;
+    /** @var string */
     public $role;
+    /** @var string */
     public $type;
+    /** @var string */
     public $deliverySttings;
+    /** @var string */
     public $kind;
-
-    /**
-     * function constructor
-     */
-    public function __construct(
-        $email,
-        $role,
-        $type,
-        $deliverySttings,
-        $kind
-    )
-    {
-        $this->email = $email;
-        $this->role = $role;
-        $this->type = $type;
-        $this->deliverySttings = $deliverySttings;
-        $this->kind = $kind;
-
-    }
-
-    /**
-     * function jsonSerialize
-     *
-     * @return mixed
-     */
-    public function jsonSerialize()
-    {
-        return[
-            'email' => $this->email,
-            'role' => $this->role,
-            'type' => $this->type,
-            'deliverySettings' => $this->deliverySttings,
-            'kind' => $this->kind
-        ];
-    }
 }
