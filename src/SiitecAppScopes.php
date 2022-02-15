f134346f65de0fd074facab3f6c7c79454a4d366
Francerz <francerzam@hotmail.com>
2022-02-15 09:28:50 -0600

    ADD SiitecAppScopes::POST_USUARIO

--- src/SiitecAppScopes.php
+++ src/SiitecAppScopes.php
@@ -6,4 +6,5 @@ interface SiitecAppScopes
 {
     public const POST_APP_EMAIL = 'app.email:post';
     public const POST_ESCOLARES_KARDEX = 'escolares.kardex:post';
+    public const POST_USUARIO = 'app.usuario:post';
 }
