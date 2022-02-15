a8d8959b17f6459a7c58e7f25c1dc2391402e8b1
Francerz <francerzam@hotmail.com>
2022-02-15 14:19:29 -0600

    ADD Aspirante::fromHttpMessage and jsonSerialize

--- src/Model/App/Usuarios/Aspirante.php
+++ src/Model/App/Usuarios/Aspirante.php
@@ -2,13 +2,16 @@
 
 namespace ITColima\SiitecApi\Model\App\Usuarios;
 
-class Aspirante
+use Francerz\Http\Utils\HttpHelper;
+use JsonSerializable;
+use Psr\Http\Message\MessageInterface;
+
+class Aspirante implements JsonSerializable
 {
     // usuarios
     public $id_usuario;
     public $usuario;
     public $password;
-    public $password_method;
 
     // aspirantes
     public $id_aspirante;
@@ -19,4 +22,35 @@ class Aspirante
     // usuarios_correos
     public $correo;
     public $correo_verify;
+
+    public static function fromHttpMessage(MessageInterface $message)
+    {
+        $data = HttpHelper::getContent($message);
+        $aspirante = new self();
+        $aspirante->id_usuario = $data->id_usuario ?? null;
+        $aspirante->usuario = $data->usuario ?? null;
+        $aspirante->password = $data->password ?? null;
+        $aspirante->id_aspirante = $data->id_aspirante ?? null;
+        $aspirante->nombres = $data->nombres ?? null;
+        $aspirante->apellido1 = $data->apellido1 ?? null;
+        $aspirante->apellido2 = $data->apellido2 ?? null;
+        $aspirante->correo = $data->correo ?? null;
+        $aspirante->correo_verify = $data->correo_verify ?? null;
+        return $aspirante;
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            'id_usuario' => $this->id_usuario,
+            'usuario' => $this->usuario,
+            'password' => $this->password,
+            'id_aspirante' => $this->id_aspirante,
+            'nombres' => $this->nombres,
+            'apellido1' => $this->apellido1,
+            'apellido2' => $this->apellido2,
+            'correo' => $this->correo,
+            'correo_verify' => $this->correo_verify
+        ];
+    }
 }
