a7c337c26db1556d9bbb6b45149332bc254a9ad9
Francerz <francerzam@hotmail.com>
2022-02-18 11:17:27 -0600

    CHG SiitecAppScopes::POST_USUARIO_ASPIRANTE

--- src/SiitecAppScopes.php
+++ src/SiitecAppScopes.php
@@ -6,5 +6,5 @@ interface SiitecAppScopes
 {
     public const POST_APP_EMAIL = 'app.email:post';
     public const POST_ESCOLARES_KARDEX = 'escolares.kardex:post';
-    public const POST_USUARIO = 'app.usuario:post';
+    public const POST_USUARIO_ASPIRANTE = 'app.usuario.aspirante:post';
 }
