03e8d4568dbe72e207e7e9541b312f0833fa1106
Francerz <francerzam@hotmail.com>
2022-03-29 11:45:15 -0600

    ADD preinscripciones/aspirantes/{id} PATCH

--- src/Resources/Preinscripciones/AspirantesResource.php
+++ src/Resources/Preinscripciones/AspirantesResource.php
@@ -2,8 +2,10 @@
 
 namespace ITColima\SiitecApi\Resources\Preinscripciones;
 
+use Francerz\Http\Utils\Constants\MediaTypes;
 use Francerz\Http\Utils\HttpHelper;
 use ITColima\SiitecApi\AbstractResource;
+use ITColima\SiitecApi\Model\Preinscripciones\Aspirante;
 
 class AspirantesResource extends AbstractResource
 {
@@ -11,6 +13,17 @@ class AspirantesResource extends AbstractResource
     {
         $this->requiresClientAccessToken();
         $response = $this->_get("/preinscripciones/aspirantes/{$aspirante_id}");
+        return HttpHelper::getContent($response);
+    }
+
+    public function patch($id_aspirante, Aspirante $aspirante)
+    {
+        $this->requiresClientAccessToken();
+        $response = $this->_patch(
+            "/preinscripciones/aspirantes/{$id_aspirante}",
+            $aspirante,
+            MediaTypes::APPLICATION_JSON
+        );
         return HttpHelper::getContent($response);
     }
 }
