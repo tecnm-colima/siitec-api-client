16ed6933774eaf7f9f6e31466087932b1c0b74f7
Francerz <francerzam@hotmail.com>
2022-03-30 11:47:58 -0600

    ADD consulta pagos proceso

---
+++ src/Resources/Pagos/ProcesosResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ITColima\SiitecApi\Resources\Pagos;
+
+use Francerz\Http\Utils\HttpHelper;
+use Francerz\PowerData\Objects;
+use ITColima\SiitecApi\AbstractResource;
+use ITColima\SiitecApi\Model\Pagos\Pago;
+
+class ProcesosResource extends AbstractResource
+{
+    public function getPagos($id_proceso, array $params = [])
+    {
+        $this->requiresClientAccessToken();
+        $response = $this->_get("/pagos/procesos/{$id_proceso}/pagos", $params);
+        $rows = HttpHelper::getContent($response);
+        foreach ($rows as &$row) {
+            $row = Objects::cast($row, Pago::class);
+        }
+        return $rows;
+    }
+}
