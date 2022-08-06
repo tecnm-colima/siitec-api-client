22f9aa40cacd0e75f88b55165f35d3b166abc499
Francerz <francerzam@hotmail.com>
2022-08-06 15:44:27 -0500

    ADD InscripcionesResource::inscribirBatch() method

--- src/Resources/Escolares/InscripcionesResource.php
+++ src/Resources/Escolares/InscripcionesResource.php
@@ -2,8 +2,11 @@
 
 namespace ITColima\SiitecApi\Resources\Escolares;
 
+use Francerz\Http\Utils\Constants\MediaTypes;
 use Francerz\Http\Utils\HttpHelper;
+use InvalidArgumentException;
 use ITColima\SiitecApi\AbstractResource;
+use ITColima\SiitecApi\Model\Escolares\Estudiante;
 use ITColima\SiitecApi\Model\Escolares\Inscripcion;
 use ITColima\SiitecApi\Model\Escolares\InscripcionAspirante;
 
@@ -26,6 +29,30 @@ class InscripcionesResource extends AbstractResource
     {
         $this->requiresClientAccessToken();
         $response = $this->_put("/escolares/inscripciones/aspirantes/{$inscr->id_aspirante}/{$inscr->id_periodo}", null);
+        return HttpHelper::getContent($response);
+    }
+
+    /**
+     * @param int|string $periodo_id
+     * @param Estudiante[] $estudiantes
+     */
+    public function inscribirBatch($periodo_id, array $estudiantes)
+    {
+        foreach ($estudiantes as $e) {
+            if (!$e instanceof Estudiante) {
+                throw new InvalidArgumentException("Each item in \$estudiantes MUST be an Estudiante type object");
+            }
+            if (!isset($e->aspirante_id) || !isset($e->curp)) {
+                throw new InvalidArgumentException("Aspirantes MUST have aspirante_id and CURP.");
+            }
+        }
+
+        $this->requiresClientAccessToken();
+        $response = $this->_post(
+            "/escolares/periodos/{$periodo_id}/inscripciones/batch",
+            $estudiantes,
+            MediaTypes::APPLICATION_JSON
+        );
         return HttpHelper::getContent($response);
     }
 }
