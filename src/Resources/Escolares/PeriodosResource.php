46bd867bd463915df540cba13a240f8bbcdb4e1d
Francerz <francerzam@hotmail.com>
2023-01-17 10:36:50 -0600

    ADD PeriodosResource::getCarrera

--- src/Resources/Escolares/PeriodosResource.php
+++ src/Resources/Escolares/PeriodosResource.php
@@ -3,7 +3,10 @@
 namespace ITColima\SiitecApi\Resources\Escolares;
 
 use Francerz\Http\Utils\HttpHelper;
+use Francerz\JsonTools\JsonEncoder;
 use ITColima\SiitecApi\AbstractResource;
+use ITColima\SiitecApi\Model\Escolares\Periodo;
+use ITColima\SiitecApi\Model\Escolares\PeriodoCarrera;
 
 class PeriodosResource extends AbstractResource
 {
@@ -13,13 +16,13 @@ class PeriodosResource extends AbstractResource
      * @param array $params
      *  - fin_desde: Periodos que terminen a partir de la fecha proporcionada
      *  - inicio_desde: Periodos que inician a partir de la fecha proporcionada.
-     * @return array
+     * @return Periodo[]
      */
     public function getAll(array $params = [])
     {
         $this->requiresAccessToken(false);
         $response = $this->protectedGet('/escolares/periodos', $params);
-        return HttpHelper::getContent($response);
+        return JsonEncoder::decode((string)$response->getBody(), Periodo::class);
     }
 
     public function getById($periodo_id, array $params = [])
@@ -32,10 +35,26 @@ class PeriodosResource extends AbstractResource
         return HttpHelper::getContent($response);
     }
 
+    /**
+     * @param array $params
+     * @return Periodo
+     */
     public function getCurrent(array $params = [])
     {
         $this->requiresAccessToken(false);
         $response = $this->protectedGet('/escolares/periodos/@current', $params);
-        return HttpHelper::getContent($response);
+        return JsonEncoder::decode((string)$response->getBody(), Periodo::class);
+    }
+
+    /**
+     * @param int|string $periodo_id
+     * @param int|string $carrera_id
+     * @return PeriodoCarrera
+     */
+    public function getCarrera($periodo_id, $carrera_id)
+    {
+        $this->requiresAccessToken(false);
+        $response = $this->protectedGet("/escolares/periodos/{$periodo_id}/carreras/{$carrera_id}");
+        return JsonEncoder::decode((string)$response->getBody(), PeriodoCarrera::class);
     }
 }
