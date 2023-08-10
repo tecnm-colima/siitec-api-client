e081bf3dcb6f7e9d9744610cb63e367d67057ef8
Francerz <francerzam@hotmail.com>
2023-08-10 14:12:40 -0600

    ADD ConvocatoriasResource->getInscritos

---
+++ src/Resources/Preinscripciones2/ConvocatoriasRescource.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ITColima\SiitecApi\Resources\Preinscripciones2;
+
+use Francerz\JsonTools\JsonEncoder;
+use ITColima\SiitecApi\AbstractResource;
+use ITColima\SiitecApi\Model\Preinscripciones2\Inscrito;
+
+class ConvocatoriasResource extends AbstractResource
+{
+    public function getInscritos($id_convocatoria)
+    {
+        $response = $this->protectedGet("/preinscripciones2/convocatorias/{$id_convocatoria}/inscritos");
+        return JsonEncoder::decode((string)$response->getBody(), Inscrito::class);
+    }
+}
